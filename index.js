8062dd0156575841aef28dfcca327a7145308311
Harley <51787264+Harleythetech@users.noreply.github.com>
2024-07-11 22:46:24 +0800

    Simple implementation v1 :)

---
+++ index.js
@@ -0,0 +1,24 @@
+async function Grabdata() {
+    var APIKEY = 'https://osutrack-api.ameo.dev/peak?user=21943340&mode=0';
+
+    try {
+        const Getapi = await fetch(APIKEY, { method: "GET" });
+        if (!Getapi) {
+            document.getElementById("api").innerHTML = "No data response from osutrack-api, try again later. :(";
+        } else {
+            const jsonapi = await Getapi.json();
+            const apitext = JSON.stringify(jsonapi);
+            const modtext = apitext.replace("]", '');
+            const finalapi = modtext.replace("[", "");
+            const response = JSON.parse(finalapi);
+            const FeedData = "Global rank: " + response.best_global_rank + ", " + "Accuracy: " + response.best_accuracy
+            document.getElementById("api").innerHTML = FeedData;
+            console.log(response);
+        }
+
+    } catch (exception) {
+        document.getElementById("api").innerHTML = "No data response from osutrack-api, try again later. :(";
+        console.error(exception);
+    }
+}
+
